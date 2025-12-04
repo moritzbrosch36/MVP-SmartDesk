7a9e111d1094bbe9229a1d3106a09aa85c5e3361
moritzbrosch36 <moritz.brosch23@googlemail.com>
2025-12-04 13:57:33 +0100

    Creation of the folder 'crud' and the subfiles of 'crud_engine.py' and 'generic_crud.py'. In the first step, I wrote the code from 'genetic_crud.py'.

---
+++ crud/generic_crud.py
@@ -0,0 +1,65 @@
+from models.db_blueprint import db
+from sqlalchemy.inspection import inspect
+
+# Hilfsfunktion: SQLAlchemy Object -> dict
+def to_dict(instance):
+    return {c.key: getattr(instance, c.key)
+            for c in inspect(instance).mapper.column_attrs}
+
+
+# ------------------------------------------
+# CREATE
+# ------------------------------------------
+def crud_create(model, data: dict):
+    obj = model(**data)
+    db.session.add(obj)
+    db.session.commit()
+    return to_dict(obj)
+
+# ------------------------------------------
+# READ
+# ------------------------------------------
+def crud_read(model, filters: dict = None):
+    query = model.query
+
+    if filters:
+        for key, value in filters.items():
+            if hasattr(model, key):
+                query = query.filter(getattr(model, key) == value)
+
+    return [to_dict(x) for x in query.all()]
+
+# ------------------------------------------
+# GET ONE
+# ------------------------------------------
+def crud_get_one(model, item_id):
+    obj = model.query.get(item_id)
+    if obj:
+        return to_dict(obj)
+    return None
+
+# ------------------------------------------
+# UPDATE
+# ------------------------------------------
+def crud_update(model, item_id, data: dict):
+    obj = model.query.get(item_id)
+    if not obj:
+        return None
+
+    for key, value in data.items():
+        if hasattr(obj, key):
+            setattr(obj, key, value)
+
+    db.session.commit()
+    return to_dict(obj)
+
+# ------------------------------------------
+# DELETE
+# ------------------------------------------
+def crud_delete(model, item_id):
+    obj = model.query.get(item_id)
+    if not obj:
+        return False
+    db.session.delete(obj)
+    db.session.commit()
+    return True
