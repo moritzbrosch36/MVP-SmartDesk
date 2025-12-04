88448936c1dd711ac30885db5472d79234404180
moritzbrosch36 <moritz.brosch23@googlemail.com>
2025-12-04 13:35:12 +0100

    The SQL database is now correctly stored in the database folder. The corresponding adjustments have been made to main.py.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 from models.db_blueprint import db, generate_models
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///smartdesk.db"
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config["SQLALCHEMY_DATABASE_URI"] = \
+        f"sqlite:///{os.path.join(basedir, 'database/smartdesk.db')}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
